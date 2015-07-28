fe1082da97fed0201b67bf60c6a0a59b5c9fd57a
manojthomas <manojttm@gmail.com>
2015-07-28 14:23:31 +0530

    mail updates

---
+++ Leankit_Rally_Integration/WebContent/index.jsp
@@ -0,0 +1,128 @@
+<%@page import="java.io.FileInputStream"%>
+<%@page import="javax.mail.Message"%>
+<%@page import="javax.mail.internet.InternetAddress"%>
+<%@page import="com.rallydev.rest.util.Ref"%>
+<%@page import="com.rallydev.rest.util.QueryFilter"%>
+<%@page import="com.rallydev.rest.util.Fetch"%>
+<%@page import="java.net.URI"%>
+<%@page import="java.io.IOException"%>
+<%@page import="javax.mail.MessagingException"%>
+<%@page import="javax.mail.Transport"%>
+<%@page import="javax.mail.internet.MimeMessage"%>
+<%@page import="javax.mail.PasswordAuthentication"%>
+<%@page import="javax.mail.Session"%>
+<%@page import="java.util.Properties"%>
+<%@page import="com.rallydev.rest.response.UpdateResponse"%>
+<%@page import="com.rallydev.rest.request.UpdateRequest"%>
+<%@page import="com.google.gson.JsonObject"%>
+<%@page import="com.rallydev.rest.response.QueryResponse"%>
+<%@page import="com.rallydev.rest.request.QueryRequest"%>
+<%@page import="com.rallydev.rest.RallyRestApi"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+RallyRestApi restApi = null;
+
+try {
+	final Properties props = new Properties();
+	props.load(new FileInputStream("/var/lib/openshift/55b2118d5973ca1a2f000023/app-root/runtime/dependencies/jbossews/webapps/messages.properties"));
+	restApi = new RallyRestApi(new URI(
+			props.getProperty("UpdateRallyTicket.URL")),
+			props.getProperty("UpdateRallyTicket.key"));
+
+	String formattedID = request.getParameter("ticketID");
+
+	QueryRequest rallyRequest = null;
+	if (formattedID.substring(0, 2).equalsIgnoreCase("DE")) {
+		rallyRequest = new QueryRequest("Defect");
+	} else {
+		rallyRequest = new QueryRequest("HierarchicalRequirement");
+	}
+
+	rallyRequest.setFetch(new Fetch("FormattedID", "Name",
+			request.getParameter("fieldName")));
+	rallyRequest.setQueryFilter(new QueryFilter("FormattedID", "=",
+			formattedID));
+
+	QueryResponse rallyQueryResponse = restApi.query(rallyRequest);
+	JsonObject rallyJsonObject = rallyQueryResponse.getResults().get(0)
+			.getAsJsonObject();
+	String rallyRef = Ref.getRelativeRef(rallyJsonObject
+			.get("_ref")
+			.toString()
+			.substring(
+					rallyJsonObject.get("_ref").toString()
+							.indexOf("/v2.0") + 5).replace("\"", ""));
+
+	rallyJsonObject = new JsonObject();
+	rallyJsonObject.addProperty(request.getParameter("fieldName"),
+			props.getProperty("UpdateRallyTicket."+request.getParameter("columnTo")));
+
+	UpdateRequest updateRequest = new UpdateRequest(rallyRef,
+			rallyJsonObject);
+	UpdateResponse updateResponse = restApi.update(updateRequest);
+	rallyJsonObject = updateResponse.getObject();
+
+	if (!updateResponse.wasSuccessful()) {
+		%>Update Failed<%
+		String to = props
+				.getProperty("UpdateRallyTicket.recipientList");
+
+		// Get the session object
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session1 = Session.getDefaultInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(
+								props.getProperty("UpdateRallyTicket.mailFrom"),
+								props.getProperty("UpdateRallyTicket.mailFromPassword"));
+					}
+				});
+
+		// compose message
+		try {
+			MimeMessage message = new MimeMessage(session1);
+			message.setFrom(new InternetAddress(props
+					.getProperty("UpdateRallyTicket.mailFrom")));
+			message.addRecipient(Message.RecipientType.TO,
+					new InternetAddress(to));
+			message.setSubject("Leankit 2 Rally Update FAILED");
+			message.setText(String.format("Failed ticket = %s",
+					formattedID));
+
+			// send message
+			Transport.send(message);
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+
+	}else {%>Update Success<%}
+
+} catch (Exception e) {
+	e.printStackTrace();
+} finally {
+	try {
+		restApi.close();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+}
+%>
+
+</body>
+</html>
--- Leankit_Rally_Integration/src/com/tui/uk/leankit/servlet/Test.java
+++ Leankit_Rally_Integration/src/com/tui/uk/leankit/servlet/Test.java
@@ -1,5 +1,6 @@
 package com.tui.uk.leankit.servlet;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -31,53 +32,44 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RallyRestApi restApi = null;
+		final Properties props = new Properties();
 		try {
-			restApi = new RallyRestApi(new URI(Messages.getString("UpdateRallyTicket.URL")), Messages.getString("UpdateRallyTicket.key"));
-		} catch (URISyntaxException e) {
+			props.load(new FileInputStream("messages.properties"));
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} //$NON-NLS-1$ //$NON-NLS-2$
+			e1.printStackTrace();
+		}
 		try {
-			//String ref = Ref.getRelativeRef("/defect/37378444620");
-			//System.out.println(String.format("\nReading defect %s...", ref));
-	        //GetRequest getRequest = new GetRequest(ref);
-	        //GetResponse getResponse = null;
-			//getResponse = restApi.get(getRequest);
+			restApi = new RallyRestApi(new URI(props.getProperty("UpdateRallyTicket.URL")), props.getProperty("UpdateRallyTicket.key"));
 
-	        //JsonObject obj = getResponse.getObject();
-	        //System.out.println(String.format("Read defect. Name = %s, State = %s",
-	                //obj.get("Name").getAsString(), obj.get("State").getAsString()));
-			String formattedID = "DE43139";
+			String formattedID = "US25668";
+
 			QueryRequest rallyRequest = null;
 			if (formattedID.substring(0, 2).equalsIgnoreCase("DE")) {
 				rallyRequest = new QueryRequest("Defect");
 			}
-			else
+			else{
 				rallyRequest = new QueryRequest("HierarchicalRequirement");
+			}
 
-			rallyRequest.setFetch(new Fetch("FormattedID","Name","c_KanbanState"));
+			rallyRequest.setFetch(new Fetch("FormattedID","Name","c_WPSKanbanState"));
 			rallyRequest.setQueryFilter(new QueryFilter("FormattedID", "=", formattedID));
 
-
-
-			/*String storyFormattedID = "US25668";
-			QueryRequest storyRequest = new QueryRequest("HierarchicalRequirement");
-			storyRequest.setFetch(new Fetch("FormattedID","Name","c_KanbanState"));
-			storyRequest.setQueryFilter(new QueryFilter("FormattedID", "=", storyFormattedID));*/
 			QueryResponse rallyQueryResponse = restApi.query(rallyRequest);
 			JsonObject rallyJsonObject = rallyQueryResponse.getResults().get(0).getAsJsonObject();
-			String storyRef = Ref.getRelativeRef(rallyJsonObject.get("_ref").toString().substring(rallyJsonObject.get("_ref").toString().indexOf("/v2.0")+5).replace("\"", ""));
-			//storyJsonObject.addProperty("Discussion", "Able to update defects via API");
+			String rallyRef = Ref.getRelativeRef(rallyJsonObject.get("_ref").toString().substring(rallyJsonObject.get("_ref").toString().indexOf("/v2.0")+5).replace("\"", ""));
+
 			rallyJsonObject = new JsonObject();
-			rallyJsonObject.addProperty("c_KanbanState", "Dependencies");
-			UpdateRequest updateRequest = new UpdateRequest(storyRef, rallyJsonObject);
+			rallyJsonObject.addProperty("c_WPSKanbanState", "Prioritised");
+			UpdateRequest updateRequest = new UpdateRequest(rallyRef, rallyJsonObject);
 	        UpdateResponse updateResponse =  restApi.update(updateRequest);
 			rallyJsonObject = updateResponse.getObject();
+
 			if(!updateResponse.wasSuccessful()){
-				String to="manojttm@gmail.com";//change accordingly
+				String to=props.getProperty("UpdateRallyTicket.recipientList");
 
 				  //Get the session object
-				  Properties props = new Properties();
+
 				  props.put("mail.smtp.host", "smtp.gmail.com");
 				  props.put("mail.smtp.socketFactory.port", "465");
 				  props.put("mail.smtp.socketFactory.class",
@@ -88,22 +80,22 @@ public class Test {
 				  Session session = Session.getDefaultInstance(props,
 				   new javax.mail.Authenticator() {
 				   protected PasswordAuthentication getPasswordAuthentication() {
-				   return new PasswordAuthentication("manojttm@gmail.com","");//change accordingly
+				   return new PasswordAuthentication(props.getProperty("UpdateRallyTicket.mailFrom"),props.getProperty("UpdateRallyTicket.mailFromPassword"));
 				   }
 				  });
 
 				  //compose message
 				  try {
 				   MimeMessage message = new MimeMessage(session);
-				   message.setFrom(new InternetAddress("manojttm@gmail.com"));//change accordingly
+				   message.setFrom(new InternetAddress(props.getProperty("UpdateRallyTicket.mailFrom")));
 				   message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
-				   message.setSubject("Hello");
-				   message.setText("Testing.......");
+				   message.setSubject("Leankit 2 Rally Update FAILED");
+				   message.setText(String.format("Failed ticket = %s", formattedID));
 
 				   //send message
 				   Transport.send(message);
 
-				   System.out.println("message sent successfully");
+				   //System.out.println("message sent successfully");
 
 				  } catch (MessagingException e) {throw new RuntimeException(e);}
 
@@ -119,6 +111,7 @@ public class Test {
 				e.printStackTrace();
 			}
 		}
+
 
 
 	}
